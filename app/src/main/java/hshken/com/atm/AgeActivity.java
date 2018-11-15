bd3e1a4febf89efc78130cef670aeb22f40881ac
hshken <hshken@livemail.tw>
2018-11-15 20:30:05 +0800

    homepractice

--- app/src/main/java/hshken/com/atm/AgeActivity.java
+++ app/src/main/java/hshken/com/atm/AgeActivity.java
@@ -1,13 +1,30 @@
 package hshken.com.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AgeActivity extends AppCompatActivity {
-
+    private EditText edage;
+    private String age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+    }
+    public void next(View view){
+        edage = findViewById(R.id.ed_age);
+        age = edage.getText().toString();
+        getSharedPreferences("AGE",MODE_PRIVATE)
+                .edit()
+                .putString("AGE",age)
+                .apply();
+        Intent gender = new Intent(this,GenderActivity.class);
+        startActivity(gender);
+    }
+    public void back(View view){
+        finish();
     }
 }
--- app/src/main/java/hshken/com/atm/GenderActivity.java
+++ app/src/main/java/hshken/com/atm/GenderActivity.java
@@ -1,13 +1,31 @@
 package hshken.com.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class GenderActivity extends AppCompatActivity {
-
+    private  EditText edgender;
+    private String gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender);
+    }
+    public void next(View view){
+        edgender = findViewById(R.id.ed_gender);
+        gender = edgender.getText().toString();
+        getSharedPreferences("GENDER",MODE_PRIVATE)
+                .edit()
+                .putString("GENDER",gender)
+                .apply();
+        Intent mainActivity = new Intent(this,MainActivity.class);
+        startActivity(mainActivity);
+    }
+    public void back(View view){
+        finish();
+
     }
 }
